2e2c6d85a54f94b9c36795195da5f0b8652f4495
john <hejiongsg@gmail.com>
2018-03-20 19:16:07 +0800

    List and tree operations

--- SolutionClasses.h
+++ SolutionClasses.h
@@ -7,6 +7,8 @@
 
 #include <iostream>
 #include <set>
+#include <queue>
+
 using namespace std;
 
 template <typename T>
@@ -24,8 +26,11 @@ public:
     ListNode<T>* construct(int len);
     ListNode<T>* insert(ListNode<T>* head, T val, int index);
     ListNode<T>* remove(ListNode<T>* head, int index);
+    ListNode<T>* reverseList(ListNode<T>* head);
     void printList(ListNode<T>* head);
+    void printReverseList(ListNode<T>* head);
     int getListLen(ListNode<T>* head);
+    bool hasCircle(ListNode<T>* head);
 };
 
 template <typename T>
@@ -53,11 +58,20 @@ public:
 
     void insert(T key);
     void insert(T key, TreeNode<T>*& root);
-    void del(T key);
+    void del(T key, TreeNode<T>*& root);
     void preOrder(TreeNode<T>* root);
     void inOrder(TreeNode<T>* root);
     void postOrder(TreeNode<T>* root);
     void layerOrder(TreeNode<T>* root);
+    bool isFound(T key, TreeNode<T>* root);
+    TreeNode<T>* findMin(TreeNode<T>* root);
+    int countNumOfNodes(TreeNode<T>* root);
+    int heightOfTree(TreeNode<T>* root);
+
+    TreeNode<T>* transformToList(TreeNode<T>* root);
+
+private:
+    TreeNode<T>* transformToListImpl(TreeNode<T>* root, TreeNode<T>*& pHead, TreeNode<T>*& pTail);
 };
 
 // Solution_0
@@ -124,6 +138,28 @@ ListNode<T>* Solution_0_List<T>::remove(ListNode<T>* head, int index) {
 }
 
 template <typename T>
+ListNode<T>* Solution_0_List<T>::reverseList(ListNode<T> *head) {
+    ListNode<T>* newHead = nullptr;
+
+    if(head == nullptr || head->next == nullptr)
+        newHead = head;
+    else {
+        ListNode<T>* prevPtr = head;
+        ListNode<T>* nextPtr = prevPtr->next;
+        head->next = nullptr;
+
+        while (nextPtr != nullptr) {
+            ListNode<T>* tempPtr = nextPtr->next;
+            nextPtr->next = prevPtr;
+            prevPtr = nextPtr;
+            nextPtr = tempPtr;
+        }
+        newHead = prevPtr;
+    }
+    return newHead;
+}
+
+template <typename T>
 void Solution_0_List<T>::printList(ListNode<T> *head) {
     if (head == NULL) {
         cout << "Empty list" << endl;
@@ -138,6 +174,14 @@ void Solution_0_List<T>::printList(ListNode<T> *head) {
 }
 
 template <typename T>
+void Solution_0_List<T>::printReverseList(ListNode<T> *head) {
+    if(head != nullptr) {
+        printReverseList(head->next);
+        cout << head->val << " ";
+    }
+}
+
+template <typename T>
 int Solution_0_List<T>::getListLen(ListNode<T> *head) {
     ListNode<T>* ptr = head;
     int len = 0;
@@ -146,6 +190,21 @@ int Solution_0_List<T>::getListLen(ListNode<T> *head) {
         ptr = ptr->next;
     }
     return len;
+}
+
+template <typename T>
+bool Solution_0_List<T>::hasCircle(ListNode<T> *head) {
+    if(head == nullptr || head->next == nullptr) return false;
+    else {
+        ListNode<T>* fastPtr, *slowPtr = head;
+        while(fastPtr != nullptr && fastPtr->next != nullptr) {
+            slowPtr = slowPtr->next;
+            fastPtr = fastPtr->next->next;
+            if(fastPtr == slowPtr)
+                return true;
+        }
+        return false;
+    }
 }
 
 // Solution_1
@@ -165,31 +224,27 @@ void Solution_1_Tree<T>::insert(T key, TreeNode<T>*& root) {
 }
 
 template <typename T>
-void Solution_1_Tree<T>::inOrder(TreeNode<T>* root) {
+void Solution_1_Tree<T>::preOrder(TreeNode<T>* root) {
     if(this->root == nullptr) {
         cout << "Empty tree!" << endl;
     } else {
-        if(root->left != nullptr) {
-            inOrder(root->left);
-        }
-        cout << root->key << " ";
-        if(root->right != nullptr) {
-            inOrder(root->right);
+        if (root != nullptr) {
+            cout << root->key << " ";
+            preOrder(root->left);
+            preOrder(root->right);
         }
     }
 }
 
 template <typename T>
-void Solution_1_Tree<T>::preOrder(TreeNode<T>* root) {
+void Solution_1_Tree<T>::inOrder(TreeNode<T>* root) {
     if(this->root == nullptr) {
         cout << "Empty tree!" << endl;
     } else {
-        cout << root->key << " ";
-        if(root->left != nullptr) {
-            preOrder(root->left);
-        }
-        if(root->right != nullptr) {
-            preOrder(root->right);
+        if(root != nullptr) {
+            inOrder(root->left);
+            cout << root->key << " ";
+            inOrder(root->right);
         }
     }
 }
@@ -199,19 +254,129 @@ void Solution_1_Tree<T>::postOrder(TreeNode<T> *root) {
     if(this->root == nullptr) {
         cout << "Empty tree!" << endl;
     } else {
-        if(root->left != nullptr) {
+        if(root != nullptr) {
             postOrder(root->left);
-        }
-        if(root->right != nullptr) {
             postOrder(root->right);
+            cout << root->key << " ";
         }
-        cout << root->key << " ";
     }
 }
 
 template <typename T>
-void Solution_1_Tree<T>::del(T key) {
-
+bool Solution_1_Tree<T>::isFound(T key, TreeNode<T>* root) {
+    if (this->root == nullptr) return false;
+    else if(root == nullptr) return false;
+    else if(key < root->key) return isFound(key, root->left);
+    else if(key > root->key) isFound(key, root->right);
+    else return true;
 }
 
+template <typename T>
+TreeNode<T>* Solution_1_Tree<T>::findMin(TreeNode<T> *root) {
+    if(root->left != nullptr) return findMin(root->left);
+    else return root;
+}
+
+template <typename T>
+void Solution_1_Tree<T>::del(T key, TreeNode<T>*& root) {
+    if(this->root == nullptr) cout << "Empty tree!" << endl;
+    else if(root == nullptr) cout << "Not found" << endl;
+    else if(key < root->key) del(key, root->left);
+    else if(key > root->key) del(key, root->right);
+    else {
+        if(root->left == nullptr && root->right == nullptr) {
+            TreeNode<T>* ptr = root;
+            root = nullptr;
+            delete ptr;
+        }
+        else if(root->left != nullptr && root->right != nullptr) {
+            root->key = findMin(root->right)->key;
+            del(root->key, root->right);
+        }
+        else {
+            TreeNode<T>* ptr = root;
+            root = (root->left == nullptr ? root->right : root->left);
+            delete ptr;
+        }
+    }
+}
+
+template <typename T>
+int Solution_1_Tree<T>::countNumOfNodes(TreeNode<T> *root) {
+    if(this->root == nullptr)
+        return 0;
+    else {
+//        if(root != nullptr) {
+//            return 1 + countNumOfNodes(root->left) + countNumOfNodes(root->right);
+//        }
+//        else return 0;
+        if (root->left == nullptr && root->right == nullptr)
+            return 1;
+        else if (root->left != nullptr && root->right != nullptr)
+            return 1 + countNumOfNodes(root->right) + countNumOfNodes(root->left);
+        else
+            return 1 + (root->left == nullptr ? (countNumOfNodes(root->right)) : (countNumOfNodes(root->left)));
+    }
+}
+
+template <typename T>
+int Solution_1_Tree<T>::heightOfTree(TreeNode<T> *root) {
+    if(this->root == nullptr)
+        return 0;
+    else {
+        if (root != nullptr) {
+            return 1 + max(heightOfTree(root->left), heightOfTree(root->right));
+        }
+        else return 0;
+    }
+}
+
+template <typename T>
+void Solution_1_Tree<T>::layerOrder(TreeNode<T> *root) {
+    if(this->root == nullptr)
+        cout << "Empty tree!" << endl;
+    else {
+        queue<TreeNode<T>*> queue;
+        queue.push(root);
+        while(!queue.empty()) {
+            TreeNode<T>* treeNode = queue.front();
+            cout << treeNode->key << " ";
+            queue.pop();
+
+            if (treeNode->left != nullptr)
+                queue.push(treeNode->left);
+            if (treeNode->right != nullptr)
+                queue.push(treeNode->right);
+        }
+    }
+}
+
+template <typename T>
+TreeNode<T>* Solution_1_Tree<T>::transformToListImpl(TreeNode<T> *root, TreeNode<T> *&pHead, TreeNode<T> *&pTail) {
+    if (root == nullptr) {
+        pHead = nullptr;
+        pTail = nullptr;
+    }
+    else if (root->left == nullptr && root->right != nullptr) {
+        pHead = root;
+        transformToListImpl(root->right, pHead, pTail);
+    }
+    else if (root->left != nullptr && root->right == nullptr) {
+        pHead = root->left;
+        pTail = root;
+    }
+    else {
+        transformToListImpl(root->left, pHead, root);
+        transformToListImpl(root->right, root->right, pTail);
+    }
+}
+
+template <typename T>
+TreeNode<T>* Solution_1_Tree<T>::transformToList(TreeNode<T> *root) {
+    if(this->root == nullptr) return nullptr;
+    else {
+        TreeNode<T> *pHead, *pTail = nullptr;
+        return transformToListImpl(this->root, pHead, pTail);
+    }
+}
 #endif //LINTCODE_SOLUTIONCLASSES_H
--- main.cpp
+++ main.cpp
@@ -4,18 +4,31 @@
 using namespace std;
 
 int main(int argc, char** argv) {
-    int problemID = 0;
+    int problemID = 1;
     Solution_0_List<int >* solution0 = new Solution_0_List<int>();
     Solution_1_Tree<int>* solution1 = new Solution_1_Tree<int>();
     switch(problemID) {
         case 0: {
             cout << "Solving problem " << problemID << endl;
-            ListNode<int> *list = solution0->construct(2);
+
+            cout << "construct a new list: " << endl;
+            ListNode<int> *list = solution0->construct(10);
             solution0->printList(list);
+
+            cout << "insert a new node into list: " << endl;
             list = solution0->insert(list, 100, 1);
             solution0->printList(list);
+
+            cout << "remove a node from the list: " << endl;
             list = solution0->remove(list,0);
             solution0->printList(list);
+
+            cout << "reverse the list: " << endl;
+            list = solution0->reverseList(list);
+            solution0->printList(list);
+
+            cout << "print list from the end to head: " << endl;
+            solution0->printReverseList(list);
             break;
         }
         case 1: {
@@ -33,8 +46,6 @@ int main(int argc, char** argv) {
             solution1->insert(19, solution1->root);
             solution1->insert(12, solution1->root);
 
-            cout << solution1->root->right->key << endl;
-
             cout << "visit in pre-order:";
             solution1->preOrder(solution1->root);
 
@@ -43,6 +54,34 @@ int main(int argc, char** argv) {
 
             cout << endl <<  "visit in post-order:";
             solution1->postOrder(solution1->root);
+
+            cout << endl << "find certain key: ";
+            cout << solution1->isFound(9, solution1->root);
+
+//            cout << endl << "delete key: 7";
+//            solution1->del(7, solution1->root);
+//
+            cout << endl << "visit in in-order:";
+            solution1->inOrder(solution1->root);
+
+            cout << endl << "visit in pre-order:";
+            solution1->preOrder(solution1->root);
+
+            cout << endl <<  "visit in post-order:";
+            solution1->postOrder(solution1->root);
+
+            cout << endl <<  "visit in layer-order:";
+            solution1->layerOrder(solution1->root);
+
+            cout << endl << "delete key: 19";
+            solution1->del(19, solution1->root);
+
+            cout << endl << "count of nodes in tree: ";
+            cout << solution1->countNumOfNodes(solution1->root);
+
+            cout << endl << "height of tree: ";
+            cout << solution1->heightOfTree(solution1->root);
+
             break;
         }
         default: {
